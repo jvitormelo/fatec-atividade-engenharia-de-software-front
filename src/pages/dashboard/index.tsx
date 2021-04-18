f32e3539f48f7e9b844d3c46cfc4300bea8e1130
jvitormelo <jvitor.ovidio2@hotmail.com>
2021-04-18 02:10:59 -0300

    teste desing

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,12 +6,15 @@ import { useLoadingContext, useUserContext } from '../../context/user';
 import UserResource from '../../resources/UserResource';
 
 const Dashboard = () => {
-  const { user, setUser } = useUserContext();
-  const { setLoading, loading } = useLoadingContext();
+  const { setUser } = useUserContext();
+  const { setLoading } = useLoadingContext();
   const router = useRouter();
+
   const verifyAuth = () => {
     if (!localStorage.getItem('token')) router.push('/');
   };
+
+  // const cards = ['Gerenciar', 'Resultados'];
 
   async function getUser() {
     try {
@@ -30,14 +33,29 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="bg-gray-200 flex justify-center flex-1 ">
-      {loading.active ? null : (
-        <div className="text-4xl">
-          Parabens você esta logado como
-          {user.isAdmin ? 'Administrador' : 'Usuário'}
-          !!
+    <div className="bg-gray-200 min-h-screen">
+      <div className="flex min-h-screen">
+        <div className=" flex flex-shrink p-4  ">
+          <div className="flex flex-col ">
+            <div className="mb-4">Study Wisely</div>
+            <div className="grid grid-flow-row gap-3">
+              <div className="text-sm font-medium text-gray-900 cursor-pointer ">Home</div>
+              <div className="text-sm font-medium text-gray-900 cursor-pointer">Revisar</div>
+              <div className="text-sm font-medium text-gray-900 cursor-pointer">
+                Adicionar coleção
+              </div>
+              <div className="text-sm font-medium text-gray-900 cursor-pointer">Estudar</div>
+            </div>
+          </div>
         </div>
-      )}
+        <div className="flex flex-grow bg-blue-200">
+          <div className="flex flex-grow bg-red-200 justify-center items-center ">
+            <div className="p-4 bg-white min-w-[10.23rem] min-h-[10.23rem] rounded-lg flex ">
+              card
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
